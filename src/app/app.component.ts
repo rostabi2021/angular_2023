6242d6308bbfde1f1829a4605d89653856501606
Robert Stabi <rostabi2021@gmail.com>
2023-02-03 23:06:55 +0100

    FEATURE-7 ViewChild, ViewChildre, afterViewInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'hotelinventoryapp';
 
-  role = 'Users';
+  @ViewChild('name', {static: true}) name!: ElementRef;
+
+  ngOnInit() {
+    this.name.nativeElement.innerText = 'Hilton Hotel'
+  }
+
+  // @ViewChild('user', {read: ViewContainerRef}) vcr!: ViewContainerRef;
+
+  // ngAfterViewInit(): void {
+  //   const componentRef = this.vcr.createComponent(RoomsComponent);
+  //   componentRef.instance.numberOfRooms = 50;
+  // }
 }
